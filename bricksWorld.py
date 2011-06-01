9009d427a9110d7294eec6c65cb880b55595905d
thranduil <robert.slak@gmail.com>
2011-06-01 18:43:12 +0200

    Finished creating map and price map

--- bricksWorld.py
+++ bricksWorld.py
@@ -11,6 +11,7 @@ class brickWorld():
     self.density = density
     
   ##create world with random bricks in it
+  ##1 is block,0 is empty space, 2 is finish, 3 is start 
   def createBrickWorld(self):
     for i in range(self.mapSize*self.mapSize):
       #adds border
@@ -18,22 +19,46 @@ class brickWorld():
         self.world.append(1)
       else:
         self.world.append(0)
-      #set all fields in price world to -1
-      self.priceWorld.append(-1)
+      #set all fields in price world to 0
+      self.priceWorld.append('#')
       
-    #add finish and starting point
+    #add finish and starting cell
     self.setCell(self.world,1,1,2)
     self.setCell(self.world,self.mapSize-2,self.mapSize-2,3)
     self.setCell(self.priceWorld,1,1,0)
     
-    
-    #fill the map with walls
+    #fill the map with random walls
     random.seed()
     for i in range(self.density):
       x,y = random.randint(1,self.mapSize-2),random.randint(1,self.mapSize-2)
       if x == 1 and y == 1 or x == self.mapSize-2 and y == self.mapSize-2:
         continue
       self.setCell(self.world,y,x,1)
+    self.computePrices(1,1,self.priceWorld)
+  
+  ##recurusively compute distance from finish for every cell
+  def computePrices(self,x,y,priceWorld):
+    if self.getCell(self.world,x,y) == 1:
+      return
+    if self.getCell(self.world,x-1,y) != 1:
+      if self.getCell(priceWorld,x-1,y) == '#' or self.getCell(priceWorld,x-1,y) > self.getCell(priceWorld,x,y)+1:
+        self.setCell(priceWorld,x-1,y,self.getCell(priceWorld,x,y)+1)
+        self.computePrices(x-1,y,priceWorld)
+    
+    if self.getCell(self.world,x+1,y) != 1:
+      if self.getCell(priceWorld,x+1,y) == '#' or self.getCell(priceWorld,x+1,y) > self.getCell(priceWorld,x,y)+1:
+        self.setCell(priceWorld,x+1,y,self.getCell(priceWorld,x,y)+1)
+        self.computePrices(x+1,y,priceWorld)
+    
+    if self.getCell(self.world,x,y-1) != 1:
+      if self.getCell(priceWorld,x,y-1) == '#' or self.getCell(priceWorld,x,y-1) > self.getCell(priceWorld,x,y)+1:
+        self.setCell(priceWorld,x,y-1,self.getCell(priceWorld,x,y)+1)
+        self.computePrices(x,y-1,priceWorld)
+    
+    if self.getCell(self.world,x,y+1) != 1:
+       if self.getCell(priceWorld,x,y+1) == '#' or self.getCell(priceWorld,x,y+1) > self.getCell(priceWorld,x,y)+1:
+        self.setCell(priceWorld,x,y+1,self.getCell(priceWorld,x,y)+1)
+        self.computePrices(x,y+1,priceWorld)   
   
   ##prints world
   def printWorld(self):
@@ -51,6 +76,13 @@ class brickWorld():
       else:
         print self.priceWorld[i],
   
+  ##checks if path from start to finish point exists
+  def pathExist(self):
+    if self.getCell(self.priceWorld,self.mapSize-2,self.mapSize-2) == '#':
+      return False
+    else:
+      return True
+  
   ##set value in the cell
   def setCell(self,world,row,column,value):
     world[row*self.mapSize+column] = value
@@ -60,7 +92,8 @@ class brickWorld():
     return world[row*self.mapSize+column]
   
 ##testing on one object
-w = brickWorld(10,5)
+w = brickWorld(10,25)
 w.createBrickWorld()
 w.printWorld()
+print w.pathExist()
 w.printPriceWorld()
