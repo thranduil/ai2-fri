58cadb0546721e65c6237f22241f000518c17b80
strahowsky <mike.strah@gmail.com>
2011-09-14 00:03:12 +0200

    8-puzzle distribucija rešitev in izračunan potek rešitve

--- puzzleSearch.py
+++ puzzleSearch.py
@@ -23,6 +23,7 @@ def aStarSearch(start_state, heuristic, exact_part, noise_type, noise_magnitude)
         #self.aStarCheckedNodes = len(closedList)
         #self.aStarOpenNodes = len(openList)
         #self.printPath(current)
+        print solutionPath(current)
         return
       openList.remove(current)
       closedList.add(current)
@@ -41,18 +42,6 @@ def getNeighborStates(start_position, heuristic, solution_length, exact_part, no
     noisy_h = distortHeuristic(heuristic[next], solution_length, exact_part, noise_type, noise_magnitude)
     neighbors.append(State(next,start_position,noisy_h))
   return neighbors
-
-
-def exactHeuristic():
-  f = file("db3x3.txt","r")
-  lines=f.readlines()
-  heuristic = {'123456780':0}
-  for line in lines:
-    temp = line.strip('\n').split(',')
-    if int(temp[1]) != -100:
-      heuristic[temp[0]]=int(temp[1])
-    
-  return heuristic
 
 
 def distortHeuristic(h, solution_length, exact_part, noise_type, noise_magnitude):
@@ -87,7 +76,45 @@ def calculateNoise(h, noise_type, noise_magnitude):
   return x
 
 
-## sumljenje hevristike
+def exactHeuristic():
+  f = file("db3x3.txt","r")
+  lines=f.readlines()
+  heuristic = {'123456780':0}
+  for line in lines:
+    temp = line.strip('\n').split(',')
+    if int(temp[1]) != -100:
+      heuristic[temp[0]]=int(temp[1])
+    
+  return heuristic
+
+
+def solutionsDistribution(heuristics):
+  distribution = []
+  for i in range(32):
+    distribution.append([i,0])
+  
+  for k,v in heuristics.iteritems():
+    distribution[v][1]+=1
+  
+  return distribution
+
+
+def solutionPath(ending_position):
+  path = ['',['123456780']]
+  node = ending_position
+  while node.parent != None:
+    row = list(node.position).index('0')/3
+    col = list(node.position).index('0')%3
+    rowp = list(node.parent.position).index('0')/3
+    colp = list(node.parent.position).index('0')%3
+    if rowp < row: path[0] = 'D'+path[0]
+    elif rowp > row: path[0] = 'U'+path[0]
+    elif colp < col: path[0] = 'R'+path[0]
+    elif colp > col: path[0] = 'L'+path[0]
+    path[1].insert(0,node.parent.position)
+    node = node.parent
+  
+  return path
 
 ## ida*
 
@@ -100,5 +127,5 @@ start = time.clock()
 aStarSearch(State('012345678',None,22), h, 'start', 'no_noise', 0.20)
 print 'A* alg:', time.clock()-start
 
-
+print solutionsDistribution(h)
 
